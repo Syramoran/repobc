755ffbe5a4607a12f990810441f18b27e1726e9c
Syramoran <syramoranicr@gmail.com>
2025-06-30 10:10:06 -0300

    sobre nosotros

--- src/app/sobre-nosotros/sobre-nosotros.component.ts
+++ src/app/sobre-nosotros/sobre-nosotros.component.ts
@@ -28,5 +28,26 @@ export class SobreNosotrosComponent {
     const elements: NodeListOf<Element> = document.querySelectorAll('.lazyCard');
     elements.forEach((el) => observer.observe(el));
 
+    const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
   }
 }
